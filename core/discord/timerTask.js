f0bd39303e5e699ea12fa75c374c0eb501867951
Matt <ultraman257@gmail.com>
2020-06-09 19:10:22 +0100

    Removed pointless embeds

--- core/discord/timerTask.js
+++ core/discord/timerTask.js
@@ -17,31 +17,19 @@ module.exports = scheduledTask;
 
 function randomMessage() {
 
-    try {
-        const embed = new Discord.MessageEmbed()
-            .setTitle('A Random Event!')
-            .setColor('#ffd700')
-            .setImage("https://matthewthaskins.com/img/matthew-home.png").setTimestamp();
+    let guild = client.guilds.resolveID('405171650514911243');
 
+    if (guild) {
 
-        let guild = client.guilds.resolveID('405171650514911243');
+        const chanel = client.channels.cache.get('405171650514911245');
 
-        if (guild) {
+        if (chanel) {
 
-            const chanel = client.channels.cache.get('405171650514911245');
-
-            if (chanel) {
-
-                log('debug', "I found it!");
-                chanel.send(embed)
-            }
-
+            log('debug', "I found it!");
+            chanel.send("https://matthewthaskins.com/img/matthew-home.png")
         }
-    } catch (e) {
-        log('error', e);
+
     }
-
-
 
 
 }
